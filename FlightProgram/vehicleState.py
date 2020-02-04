475923788ae8a611d27aed04d62a88200b2345b3
zlipp3032 <zlipp3032@gmail.com>
2020-02-04 14:15:22 -0500

    Guided mode with velocity input.
    Seems to have a delay of ~0.7s from desired to actual.

--- FlightProgram/vehicleState.py
+++ FlightProgram/vehicleState.py
@@ -5,8 +5,50 @@ from collections import OrderedDict
 from datetime import datetime
 
 Timeout = recordtype('Timeout',['localTimeoutTime','GCSLastRx',('peerLastRx',{})],default = None) 
+#Command = recordtype('Command',['Roll','Pitch','Throttle','Yaw',
+#                                'ux','uy','uz','vel_est_x','vel_est_y','vel_est_z',
+#                                    ('accVelZError',0),('AR',0),('VC',0),('GT',0),
+#                                    ('FC',0),('accPosXError',0),('accPosYError',0),
+#                                    ('accPosZError',0)],default = None)
 
+#PreviousState = recordtype('PreviousState',[('velPrev_x',0),
+#                                            ('velPrev_y',0),('velPrev_z',0),('accPrev_x',0),
+#                                                ('accPrev_y',0),('accPrev_z',0)], default = None)
+
+#Position = recordtype('Position',['x','y','z'], default=None)
+#Velocity = recordtype('Velocity',[('vx',0),('vy',0),('vz',0)], default = None)
+
+
+#ComputeControl = recordtype('ComputerControl',[('ux',0),('uy'),('uz',0)], default = None)
+
+#Flocking = recordtype('Flocking',['qgx','qgy','qgz',('pgx',0),('pgy',0),('pgz',0)], default = None)
+#Leader = recordtype('Leader',['qgx','qgy','qgz','pgx','pgy','pgz',('flocking',Flocking())], default = None)
+
+#Attitude = recordtype('Attitude',[('roll',0),('pitch',0),('yaw',0)], default = None)
+#AttThrust = recordtype('AttThrust',['roll','pitch','throttle','yaw'], default = None)
+
+#InitialPosition = recordtype('InitialPosition',['xo','yo','zo'], default = None)
+
+#Parameter = recordtype('Parameter',['Ts','peerTimeout','GPSTimeout',
+#                                    'expectedMAVs','isComplete','TargetAltitude','kpx','kdx',
+#                                        'kpy','kdy','kpz','kdz','targetAltitude','quadMass',
+#                                        'gravity','ku_vel','kv_vel','kw_vel','rollLimit','pitchLimit',
+#                                        'throttleLimit','stoppingDistance','desiredSpeed','isTakeoff',
+#                                        'intGain','InitPos','isLanding','isHovering','isFlocking','alpha1',
+#                                        'alpha2','beta','gamma1','gamma2','gamma3','gamma4','desiredDistance',
 Parameter = recordtype('Parameter',['IP','Ts','receivedTime','expectedMAVs','ID','isComplete','GPSTimeout','gains','config','txStateType'], default = None)
+
+#RigidBodyState = recordtype('RigidBodyState', [('startTime', None),'ID',
+#                                                   'batt','time','channels',('test',AttThrust()),
+#                                                   ('attitude',Attitude()),('RCLatch',False),('isGPS',False),
+#                                                   'lastGPSContact',('position',Position()),('velocity',Velocity()),
+#                                                   ('initPos',InitialPosition()),('command',Command()),
+#                                                   ('parameters',Parameter()),('timeout',Timeout()),
+#                                                   ('leader',Leader()),('previousState',PreviousState()),
+#                                                   'flightSeq'], default = None)
+
+#MessageState = recordtype('MessageState',['ID',('position',Position()),('velocity',Velocity()),
+#                                          ('attitude',Attitude())],default = None)
 
 Message = recordtype('Message', 'type,sendTime,content', default = None)
 
@@ -69,10 +111,9 @@ class FullVehicleState(BasicVehicleState):
         self.droneState = {'battVolt': None}
 	self.channels = {'roll': None, 'pitch': None, 'throttle': None, 'yaw': None}
 	self.avoid = {'ux': 0.0, 'uy': 0.0, 'uz':0.0}
-	self.low_pass = {'zVel_lp': 0.0, 'zVel_lp_prev': 0.0, 'uz_lp': 0.0, 'uz_lp_prev': 0.0, 'vz_des_lp': 0.0, 'vz_des_lp_prev': 0.0}
 	self.flightSeq = 0
         self.hover = {'x': None, 'y': None, 'z': None, 'lat': None,'lon': None,'alt': None}
-	self.R2T = np.matrix([[0.8, -0.8, 0.0],[0.43, 0.43, -0.9],[-0.2, -0.2, -0.2]])
+	self.R2T = np.matrix([[3.0, -3.0, 0.0],[2.0, 2.0, -3.0],[-0.0, -3.0, -1.5]])
         #self.isFlocking = False
         
     def getCSVLists(self):
@@ -112,9 +153,6 @@ class FullVehicleState(BasicVehicleState):
 
         headers += ['flightSequence']
         values += [self.flightSeq]
-
-	headers += ['zVel_lp','vz_des_lp','uz_lp']
-	values += [self.low_pass['zVel_lp'],self.low_pass['vz_des_lp'],self.low_pass['uz_lp']]
 
         headers += ['xhover', 'yhover', 'zhover','lat_hover','lon_hover','alt_hover']
         values += [self.hover['x'], self.hover['y'],self.hover['z'],self.hover['lat'],self.hover['lon'],self.hover['alt']]
